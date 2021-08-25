92d83cab8c0b03b8502fe0f38017b1dc23fee19a
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2021-08-25 14:34:41 -0500

    test 2

--- TechJobsOO/Job.cs
+++ TechJobsOO/Job.cs
@@ -20,7 +20,7 @@ namespace TechJobsOO
             nextId++;
         }
 
-        public Job(string name, Employer employerName, Location employerLocation, PositionType jobType, CoreCompetency jobCoreCompetency)
+        public Job(string name, Employer employerName, Location employerLocation, PositionType jobType, CoreCompetency jobCoreCompetency) : this()
         {
             Name = name;
             EmployerName = employerName;
@@ -28,18 +28,6 @@ namespace TechJobsOO
             JobType = jobType;
             JobCoreCompetency = jobCoreCompetency;
         }
-
-        public override bool Equals(object obj)
-        {
-            return obj is Job job &&
-                   Id == job.Id;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Id);
-        }
         // TODO: Generate Equals() and GetHashCode() methods.
-
     }
 }
--- TechJobsTests/JobTests.cs
+++ TechJobsTests/JobTests.cs
@@ -13,5 +13,16 @@ namespace TechJobsTests
             Job second_job = new Job();
             Assert.IsFalse(first_job.Id.Equals(second_job.Id));
         }
+
+        [TestMethod]
+        public void TestJobConstructorSetsAllFields()
+        {
+            Job test_job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+            Assert.AreEqual("Product tester", test_job.Name);
+            Assert.AreEqual("ACME", test_job.EmployerName.ToString());
+            Assert.AreEqual("Desert", test_job.EmployerLocation.ToString());
+            Assert.AreEqual("Quality control", test_job.JobType.ToString());
+            Assert.AreEqual("Persistence", test_job.JobCoreCompetency.ToString());
+        }
     }
 }
