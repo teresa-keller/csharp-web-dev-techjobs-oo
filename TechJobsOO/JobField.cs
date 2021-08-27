7debf0d5175847d2ddc6b548a53f1eec8ed9b912
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2021-08-27 18:33:22 -0500

    added JobField class

---
+++ TechJobsOO/JobField.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechJobsOO
+{
+    public abstract class JobField
+    {
+        public int Id { get; }
+        private static int nextId = 1;
+        public string Value { get; set; }
+
+        public JobField()
+        {
+            Id = nextId;
+            nextId++;
+            Value = "Data not available";
+        }
+
+        public JobField(string value) : this()
+        {
+            Value = value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is JobField field &&
+                   Id == field.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+    }
+}
